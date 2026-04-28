ced60dc9861666318b75fa82b20ce13c57f5aeee
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-28 15:10:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("oi")
+from functions import user_input, cabecalho, add_task
+
+if __name__ == '__main__':
+    action = user_input(cabecalho())
+    
+    if action == 1:
+        task_name = input(f"Qual tarefa deseja adicionar? ").strip()
+        add_task(task_name)
