2b86b6136653277bfe6f444916218143363bfb5b
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-01 12:13:23 -0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,85 +1,81 @@
-from pathlib import Path
 import json
-from typing import List, TypedDict
-from functions import get_date_time, clear_terminal
 
-# tipagem 
-class Task(TypedDict):
-    id: int
-    description: str
-    status: str
-    createAt: str  
-    updateAt: str
-
-
-# Pegando o caminho absoluto de "data.json"
-DIR_ROOT = Path(__file__).parent
-FILE_NAME = "data.json"
-FILE_PATH = DIR_ROOT / FILE_NAME
+from functions import clear_terminal, get_date_time, pegar_caminho_absoluto, read_data
+from types_dict import Task
 
 
 def add_task(task_name: str) -> None:
-    
-    data: List[Task] = []
+
+    data: list[Task] = []
+
+    FILE_PATH = pegar_caminho_absoluto()
 
     if FILE_PATH.exists():
-        try:
-            with open(FILE_PATH, "r", encoding="utf-8") as f:
-                data = json.load(f)
+        data = read_data()
 
-        except (json.JSONDecodeError, ValueError):
-            # Se o arquivo estiver corrompido ou vazio, começamos do zero
-            data = []
     else:
         data = []
-    
+
     date_time = get_date_time()
 
-    
-    new_task: Task = {"id": len(data) + 1, "description": task_name, "status": "todo", "createAt": date_time , "updateAt": date_time }
+    new_task: Task = {
+        "id": len(data) + 1,
+        "description": task_name,
+        "status": "todo",
+        "createdAt": date_time,
+        "updateAt": date_time,
+    }
 
     # Verificando se a tarefa que o usuário quer inserir já existe
     _exists = False
 
     for d in data:
-        if task_name in d['description']:
+        if task_name in d["description"]:
             _exists = True
             break
-    
+
     if _exists:
         clear_terminal()
         print(f"\033[31m\nA terefa {task_name} já existe!\n\033[m")
-    
+
     else:
-        data.append(new_task) # type: ignore
+        data.append(new_task)  # type: ignore
 
         with open(FILE_PATH, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
 
-        clear_terminal() 
+        clear_terminal()
         print(f"Tarefa '{task_name}' adicionada com sucesso!\n")
 
 
-def update_task():
-    ...
+def update_task(id: int):
+
+    data = read_data()
+
+    for d in data:
+        if id == d["id"]:
+            print(f"Id: {id} existe!")
+
+        else:
+            print(f"Id: {id} Não existe!")
 
 
-def delete_task():...
+def delete_task(): ...
 
 
-def mark_in_progress():...
+def mark_in_progress(): ...
 
 
-def mark_done():...
+def mark_done(): ...
 
 
-def list_all():...
+def list_all(): ...
 
 
-def list_done():...
+def list_done(): ...
 
 
-def list_todo():...
+def list_todo(): ...
 
 
-def list_in_progress():...
+def list_in_progress(): ...
